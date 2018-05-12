4265613296d7e6ef044be286fe13f338b0ed5cc9
Simon Stanford <simonjstanford@gmail.com>
2018-05-12 16:14:56 +0100

    Add project files.

---
+++ SimpleCrudSite/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleCrudSite.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int TypeID { get; set; }
+        public ProductType Type { get; set; }
+    }
+}
---
+++ SimpleCrudSite/Models/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCrudSite.Models
+{
+    public class ProductType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
+        public bool CanBeRemoved => Products.Any();
+    }
+}
